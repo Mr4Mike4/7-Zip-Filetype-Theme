b363447438e5a77998d88af12c886443515b2a9a
Mike <iv.mike@rambler.ru>
2018-05-01 16:09:40 +0300

    fix scripts

--- start_7zip.bat
+++ start_7zip.bat
@@ -1,10 +1,12 @@
 @echo off
 SET ARCFILE=7z_ico.7z
-SET PATH="7zip\Orig7z"
-SET PATH2="7zip\public\Orig7z\"
-SET ARCPATH="7zip\%ARCFILE%"
+SET PATH=7zip\Orig7z
+SET PATH2=7zip\public\Orig7z
+SET ARCPATH=%~d0%~p07zip\%ARCFILE%
 
 ResourceHacker.exe -script "%PATH%\7z32_res_script.txt"
 ResourceHacker.exe -script "%PATH%\7z64_res_script.txt"
 
-"c:\Program Files\7-Zip\7z.exe" a -t7z "%ARCPATH%" "%PATH2%" -m0=LZMA2:x=9 -x!.gitignore
+cd "%PATH2%"
+
+"c:\Program Files\7-Zip\7z.exe" a -t7z "%ARCPATH%" "." -m0=LZMA2:x=9 -x!.gitignore -x!*\.gitignore
--- start_7zip_NSIS.bat
+++ start_7zip_NSIS.bat
@@ -1,10 +1,12 @@
 @echo off
 SET ARCFILE=7z_NSIS_ico.7z
-SET PATH="7zip\NSIS"
-SET PATH2="7zip\public\NSIS\"
-SET ARCPATH="7zip\%ARCFILE%"
+SET PATH=7zip\NSIS
+SET PATH2=7zip\public\NSIS
+SET ARCPATH=%~d0%~p07zip\%ARCFILE%
 
-ResourceHacker.exe -script "%PATH%\7z32_res_script.txt"
-ResourceHacker.exe -script "%PATH%\7z64_res_script.txt"
+ResourceHacker.exe -script "%PATH%\7z32_NSIS_res_script.txt"
+ResourceHacker.exe -script "%PATH%\7z64_NSIS_res_script.txt"
 
-"c:\Program Files\7-Zip\7z.exe" a -t7z "%ARCPATH%" "%PATH2%" -m0=LZMA2:x=9 -x!.gitignore
+cd "%PATH2%"
+
+"c:\Program Files\7-Zip\7z.exe" a -t7z "%ARCPATH%" "." -m0=LZMA2:x=9 -x!.gitignore -x!*\.gitignore
